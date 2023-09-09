97fd9f0da37ac3acd79807a23738380afe28ab5e
Aymzzz <sbaiaymane6@gmail.com>
2023-09-09 05:28:00 -0700

    possible solution

--- shell.c
+++ shell.c
@@ -47,7 +47,7 @@ char *read_line(void)
 }
 
 //function execute_command
-
+/*
 void execute_command(char *input)
 {
     pid_t child_pid;
@@ -89,4 +89,47 @@ void execute_command(char *input)
         // Parent process
         wait(&status);
     }
+}*/
+void execute_command(char *input)
+{
+    pid_t child_pid;
+    int status;
+    char *token;
+    char *args[1024]; // An array to hold command and arguments
+    int i = 0;
+
+    input[strlen(input) - 1] = '\0'; // Remove newline character
+
+    // Tokenize the input
+    token = strtok(input, " ");
+    while (token != NULL)
+    {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL; // NULL-terminate the argument array
+
+    child_pid = fork();
+
+    if (child_pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (child_pid == 0)
+    {
+        // Child process
+        if (execve(args[0], args, NULL) == -1)
+        {
+            perror("execve");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        // Parent process
+        wait(&status);
+    }
 }
+
